9574af532e5d0e3899445259079622d69e08910a
TianD <tiandao_dunjian@sina.cn>
2015-07-14 18:09:01 +0800

    modify re
    analysis filename by re

--- kxMayaTool.py
+++ kxMayaTool.py
@@ -25,13 +25,14 @@ class KXTool(object):
         self.sceneName = pm.Env().sceneName().namebase
         
         if self.sceneName :
-            m = re.match(r"(?P<project_name>\w+)_(?P<episode_number>\d+)_(?P<session_number>\w+)_(?P<scene_number>\d+)_(?P<process_name>\w+)_(?P<version_number>\w+)", self.sceneName)
-            self.projectName = m.group('project_name')              #项目名称
-            self.episodeNumber =  m.group('episode_number')         #集数     
-            self.sessionNumber = m.group('session_number')              #场号  
-            self.sceneNumber = m.group('scene_number')                  #镜头号
-            self.processName = m.group('process_name')              #环节名称
-            self.versionNumber = m.group('version_number')              #版本号
+            m = re.match(r"(?P<project_name>\w+)_(?P<episode_number>\d+)_(?P<session_number>\w+)_(?P<scene_number>\d+)_(?P<process_name>\w+)", self.sceneName)
+            self.projectName = m.group('project_name')                         #项目名称
+            self.episodeNumber =  m.group('episode_number')                    #集数     
+            self.sessionNumber = m.group('session_number')                     #场号  
+            self.sceneNumber = m.group('scene_number')                         #镜头号
+            self.processName = m.group('process_name').split("_")[0]           #环节版本
+            self.versionNumber = m.group('process_name').split("_")[-1]         #版本号
+            
         else :
             pass
         
